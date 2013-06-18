8f671e95ec5bd68a251cc1e6ff04a85365430a03
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-18 09:47:49 -0700

    initial commit, includes twitter config

---
+++ config/environment.rb
@@ -0,0 +1,43 @@
+# Set up gems listed in the Gemfile.
+# See: http://gembundler.com/bundler_setup.html
+#      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Require gems we care about
+require 'rubygems'
+
+require 'uri'
+require 'pathname'
+
+require 'pg'
+require 'active_record'
+require 'logger'
+
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+require 'erb'
+require 'twitter'
+
+# Some helper constants for path-centric logic
+APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+
+APP_NAME = APP_ROOT.basename.to_s
+
+# Set up the controllers and helpers
+Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
+Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
+
+# Set up the database and models
+require APP_ROOT.join('config', 'database')
+
+twitter_setting = YAML.load_file(APP_ROOT.join('config','twitter.yaml'))
+
+Twitter.configure do |config|
+  config.consumer_key = twitter_setting["consumer_key"]
+  config.consumer_secret = twitter_setting["consumer_secret"]
+  config.oauth_token = twitter_setting["oauth_token"]
+  config.oauth_token_secret = twitter_setting["oauth_token_secret"]
+end
